5a9705fa8f1ff8a7802eeda4588ec19bd8a52bd0
Daniel-Gomes <danielgomes@ioasys.com.br>
2021-01-05 16:11:53 -0300

    fix mk_max deprecated

--- Source/LineGraph/GKLineGraph.m
+++ Source/LineGraph/GKLineGraph.m
@@ -227,7 +227,7 @@ static CGFloat kAxisMargin = 35.0; //default  25
 
 - (CGFloat)_maxValue {
     id values = [self _allValues];
-    return [[values mk_max] floatValue];
+    return [[values valueForKeyPath:@"@max.self"] floatValue];
 }
 
 - (CGFloat)_minValue {
